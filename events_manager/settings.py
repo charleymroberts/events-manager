5a339e0ba1239b56943fa69afe48df5ef5352153
Charley Roberts <charley.m.roberts@gmail.com>
2023-06-15 19:35:36 +0100

    Heroku again

--- events_manager/settings.py
+++ events_manager/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['festival-manager-2ef1a8933d4d.herokuapp.com/', 'localhost']
+ALLOWED_HOSTS = ['festival-manager-2ef1a8933d4d.herokuapp.com', 'localhost']
 
 
 # Application definition
