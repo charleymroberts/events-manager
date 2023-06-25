30b5991feb7e981698dd580faac0ef937e170830
Charley Roberts <charley.m.roberts@gmail.com>
2023-06-25 22:30:36 +0100

    Add Bootstrap styling to staff pages
    Add Bootstrap styling to staff pages and add event/performer/venue forms using django-bootstrap5

--- events_manager/settings.py
+++ events_manager/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django_bootstrap5',
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
